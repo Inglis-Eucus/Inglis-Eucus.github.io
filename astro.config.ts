e405d14ab8b4c537acdd5acf7288d947a8209ec7
HA <officialtrasher@proton.me>
2023-12-28 19:29:15 -0600

    Cambios a la estructura y referencias

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://inglis-eucus.github.io/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+  site: process.env.VERCEL_ENV === 'production' ? 'https://inglis-eucus.github.io/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3000/',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  }), icon()],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
-    },
-  },
+      exclude: ['@resvg/resvg-js']
+    }
+  }
 });
